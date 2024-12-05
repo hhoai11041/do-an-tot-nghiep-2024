f38d2e9578df6171d65668dd6cafa2d1d122ee3e
hhoai11041 <144259095+hhoai11041@users.noreply.github.com>
2024-12-05 20:06:07 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(
     origin: "http://3.107.183.6:3000",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'signal'],
   })
 );
 await connectDB();
