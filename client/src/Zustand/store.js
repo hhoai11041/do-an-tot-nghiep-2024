91fa95d6909e6fb28f6467a1e8899170edf597d7
hhoai11041 <144259095+hhoai11041@users.noreply.github.com>
2024-12-08 02:46:11 +0700

    Update store.js

--- client/src/Zustand/store.js
+++ client/src/Zustand/store.js
@@ -4,7 +4,11 @@ const useStore = create((set) => ({
   dataUser: null,
   renderApp: false,
   setRenderHeader: () => set((state) => ({ renderApp: !state.renderApp })),
-  setUserDataZus: (userData) => set({ dataUser: userData }),
+  // setUserDataZus: (userData) => set({ dataUser: userData }),
+  setUserDataZus: (userData) => {
+    console.log("Updating dataUser:", userData);
+    set({ dataUser: userData });
+  },
   clearUserData: () => set({ dataUser: null }),
 }));
 
