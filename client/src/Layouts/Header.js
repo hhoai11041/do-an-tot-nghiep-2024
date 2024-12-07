9d3b8c1ccc4aa48e75b0f92f71fc842a65d67c7f
hhoai11041 <144259095+hhoai11041@users.noreply.github.com>
2024-12-07 17:21:40 +0700

    Update Header.js

--- client/src/Layouts/Header.js
+++ client/src/Layouts/Header.js
@@ -26,7 +26,7 @@ const Header = () => {
     if(!dataUser) {
       getApi.getApiUser(setDataUser);
     }
-  }, [dataUser, renderUI, renderApp]);
+  }, [dataUser, renderApp]);
 
   return (
     <div>
